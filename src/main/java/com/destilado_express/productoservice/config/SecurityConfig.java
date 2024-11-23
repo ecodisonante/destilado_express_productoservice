a2b2c0df8669dcc280bf2e396d3ee7f0834e874c
Francisco Valdes <ecodisonante@gmail.com>
2024-11-23 18:03:45 -0300

    configurar cors y accesos

--- src/main/java/com/destilado_express/productoservice/config/SecurityConfig.java
+++ src/main/java/com/destilado_express/productoservice/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.destilado_express.productoservice.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,30 +11,57 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
     private JwtRequestFilter jwtRequestFilter; // Reutiliza o adapta el filtro de JWT
+
+    @Autowired
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .authorizeHttpRequests((req) -> req
+                        // acceso publico
+                        .requestMatchers(HttpMethod.GET, "/api/productos").permitAll()
                         // solo registrados
-                        .requestMatchers(HttpMethod.GET, "/api/productos").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/productos/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/productos/**").authenticated() // No se usa!!
                         // solo admin
                         .requestMatchers(HttpMethod.PUT, "/api/productos").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/productos").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/productos/**").hasRole("ADMIN")
                         // otros
-                        .anyRequest().authenticated())
-                .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:*");
+        config.addAllowedOrigin("http://frontend:*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
